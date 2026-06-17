83b8be9b798c288daac5d1ba0b1c82d7f0386ae4
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2026-06-17 11:30:09 +0800

    browser extension version for firefox and chrome

---
+++ browser-addon/package.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+TARGET=$1
+
+if [ "$TARGET" != "chrome" ] && [ "$TARGET" != "firefox" ]; then
+  echo "Usage: ./package.sh [chrome|firefox]"
+  exit 1
+fi
+
+echo "Packaging for $TARGET..."
+
+BUILD_DIR="build_$TARGET"
+rm -rf "$BUILD_DIR"
+mkdir -p "$BUILD_DIR"
+
+# Copy common files
+cp manifest.json popup.html popup.css icon128.png "$BUILD_DIR/"
+
+if [ "$TARGET" == "firefox" ]; then
+  echo "Adding Firefox specific settings..."
+  python3 -c "
+import json
+with open('$BUILD_DIR/manifest.json', 'r') as f:
+    data = json.load(f)
+data['browser_specific_settings'] = {
+    'gecko': {
+        'id': 'web-special-character@cindylinz.github.io'
+    }
+}
+with open('$BUILD_DIR/manifest.json', 'w') as f:
+    json.dump(data, f, indent=2)
+"
+fi
+
+ZIP_NAME="${TARGET}_addon.zip"
+rm -f "$ZIP_NAME"
+(cd "$BUILD_DIR" && zip -q -r "../$ZIP_NAME" .)
+
+echo "Done! Created $ZIP_NAME"
+rm -rf "$BUILD_DIR"
